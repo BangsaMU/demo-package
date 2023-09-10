0bdb4fe36ab26eb6b74f8d0fcf35f44e2dc2e476
samu_eyes <samu_eyes@yahoo.com>
2023-09-10 18:28:05 +0700

    add route demo1

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,10 @@ Route::get('demo', function() {
     echo 'Hello from the demo package!'.$value;
 });
 
+Route::get('demo1', function() {
+    echo 'Hello from the demo package! - 1';
+});
+
 Route::get('demo-view', function () {
     return view('demo::index');
 });
